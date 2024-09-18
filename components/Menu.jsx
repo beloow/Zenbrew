e84d9b3f47cc8bc7c237520b076cca875ec47897
beloow <quevaqueva11@gmail.com>
2024-09-18 11:43:32 +0200

    + Our menu Section

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,5 +1,109 @@
+import Separator from "./Separator";
+// Components
+import MenuItem from "./MenuItem";
+
+const menuItems = [
+  {
+    imgSrc: "/assets/menu/coffee-1.png",
+    name: "Espresso",
+    description: "Shot de café riche et audacieux",
+    price: 3.3,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-2.png",
+    name: "Cappuccino",
+    description: "Expresso avec du lait cuit à la vapeur et de la mousse",
+    price: 4.5,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-3.png",
+    name: "Latte",
+    description: "Expresso avec du lait cuit à la vapeur",
+    price: 4.2,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-4.png",
+    name: "Americano",
+    description: "Expresso avec de l'eau chaude",
+    price: 3.5,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-5.png",
+    name: "Mocha",
+    description:
+      "Expresso avec du lait cuit à la vapeur au chocolat et de la mousse",
+    price: 5.4,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-1.png",
+    name: "Macchiato",
+    description: "Expresso avec une cuillerée de mousse",
+    price: 3.75,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-2.png",
+    name: "Flat white",
+    description: "Expresso avec une cuillerée de mousse",
+    price: 4.25,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-3.png",
+    name: "Iced Coffe",
+    description: "Expresso au lait avec une légère mousse",
+    price: 3.75,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-4.png",
+    name: "Affogato",
+    description: "Espresso shot ever glace vanille",
+    price: 5.5,
+  },
+  {
+    imgSrc: "/assets/menu/coffee-5.png",
+    name: "Irish Coffee",
+    description: "Espresso shot ever glace vanille",
+    price: 6.5,
+  },
+];
+
 const Menu = () => {
-  return <section>Menu</section>;
+  return (
+    <section className="pt-12 pb-16 xl:pt-16 xl:pb-36">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-4 mb-12 xl:mb-24">
+          <h2 className="h2 text-center">Our Menu</h2>
+          <div className="mb-4">
+            <Separator bg="accent" />
+          </div>
+          <p className="text-center max-w-[620px] mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facilis
+            tempore porro et omnis sapiente fugiat cupiditate quam saepe
+            obcaecati.
+          </p>
+        </div>
+        {/* menu & button */}
+        <div className="flex flex-col items-center gap-12 xl:gap-24">
+          {/* menu */}
+          <div className="w-full grid grid-cols-1 xl:grid-cols-2 gap-y-8 gap-x-16 place-content-center">
+            {menuItems.map((item, index) => {
+              const { name, description, price, imgSrc } = item;
+              return (
+                <MenuItem
+                  name={name}
+                  description={description}
+                  price={price}
+                  imgSrc={imgSrc}
+                  key={index}
+                />
+              );
+            })}
+          </div>
+          {/* button */}
+          <button className="btn">View full menu</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Menu;
